798025f0edf03f7f61149cd398c11b694060feff
samrshi <samrshi@outlook.com>
2020-08-03 22:52:40 -0400

    day 64
    learned about notifying a view when the user selects an image and how to save an image to the user's photo library

--- 18 - Instafilter/Instafilter/Introduction/WrappingUIViewController.swift
+++ 18 - Instafilter/Instafilter/Introduction/WrappingUIViewController.swift
@@ -1,16 +1,27 @@
 //
-//  WrappingUIViewController.swift
+//  ContentView.swift
 //  Instafilter
 //
-//  Created by hawkeyeshi on 8/3/20.
+//  Created by hawkeyeshi on 8/1/20.
 //  Copyright © 2020 samrshi. All rights reserved.
 //
 
 import SwiftUI
 
+class ImageSaver: NSObject {
+    func writeToPhotoAlbum(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveError), nil)
+    }
+    
+    @objc func saveError(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        print("Save finished!")
+    }
+}
+
 struct WrappingUIViewController: View {
     @State private var image: Image?
     @State private var showingImagePicker = false
+    @State private var inputImage: UIImage?
     
     var body: some View {
         VStack {
@@ -22,14 +33,21 @@ struct WrappingUIViewController: View {
                 self.showingImagePicker = true
             }
         }
-        .sheet(isPresented: $showingImagePicker) {
-            ImagePicker()
+        .sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
+            ImagePicker(image: self.$inputImage)
         }
+    }
+    
+    func loadImage() {
+        guard let inputImage = inputImage else { return }
+        image = Image(uiImage: inputImage)
+        let imageSaver = ImageSaver()
+        imageSaver.writeToPhotoAlbum(image: inputImage)
     }
 }
 
 struct WrappingUIViewController_Previews: PreviewProvider {
     static var previews: some View {
-        WrappingUIViewController()
+        ContentView()
     }
 }
