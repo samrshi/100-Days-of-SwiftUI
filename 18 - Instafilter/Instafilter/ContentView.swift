46211e862b2a8799120c9acfee5e458d180085ee
samrshi <samrshi@outlook.com>
2020-08-06 23:30:27 -0400

    day 66
    did the challenges for Instafilter

--- 18 - Instafilter/Instafilter/ContentView.swift
+++ 18 - Instafilter/Instafilter/ContentView.swift
@@ -12,32 +12,70 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var image: Image?
-    @State private var filterIntensity = 0.5
     
     @State private var showingFilterSheet = false
     @State private var showingImagePicker = false
     @State private var inputImage: UIImage?
     @State private var processedImage: UIImage?
     
+    @State private var showingAlert = false
+    @State private var alertMessage = ""
+    
     @State private var currentFilter: CIFilter = CIFilter.sepiaTone()
+    @State private var currentFilterAsString: String = "Sepia Tone"
+    
+    @State private var filterIntensity = 0.5
+    // challenge 3
+    @State private var filterRadius = 0.5
+    @State private var filterScale = 0.5
+    
+    @State private var showingIntensitySlider = true
+    @State private var showingRadiusSlider = false
+    @State private var showingScaleSlider = false
+    
+    
     let context = CIContext()
     
     var body: some View {
         let intensity = Binding<Double> (
             get: {
                 self.filterIntensity
-            },
+        },
             set: {
                 self.filterIntensity = $0
                 self.applyProcessing()
-            }
+        }
+        )
+        
+        // challenge 3
+        let radius = Binding<Double> (
+            get: {
+                self.filterRadius
+        },
+            set: {
+                self.filterRadius = $0
+                self.applyProcessing()
+        }
+        )
+        
+        // challenge 3
+        let scale = Binding<Double> (
+            get: {
+                self.filterScale
+        },
+            set: {
+                self.filterScale = $0
+                self.applyProcessing()
+        }
         )
         
         return NavigationView {
             VStack {
                 ZStack {
-                    Rectangle()
-                        .fill(Color.secondary)
+                    if image == nil {
+                        Rectangle()
+                            .fill(Color.secondary)
+                    }
                     
                     if image != nil {
                         image?
@@ -53,21 +91,50 @@ struct ContentView: View {
                     self.showingImagePicker = true
                 }
                 
-                HStack {
-                    Text("Intensity")
+                VStack {
+                    if showingIntensitySlider {
+                        HStack {
+                            Text("Intensity")
+                            
+                            Slider(value: intensity)
+                        }
+                        .transition(.scale)
+                    }
                     
-                    Slider(value: intensity)
+                    if showingRadiusSlider {
+                        HStack {
+                            Text("Radius")
+                            
+                            Slider(value: radius)
+                        }
+                        .transition(.scale)
+                    }
+                    
+                    if showingScaleSlider {
+                        HStack {
+                            Text("Scale")
+                            
+                            Slider(value: scale)
+                        }
+                        .transition(.scale)
+                    }
                 }
                 .padding(.vertical)
                 
                 HStack {
-                    Button("Change filter") {
+                    Button(self.currentFilterAsString) {
                         self.showingFilterSheet = true 
                     }
                     
                     Spacer()
                     
                     Button("Save") {
+                        if self.image == nil {
+                            self.alertMessage = "You must select an image before saving"
+                            self.showingAlert.toggle()
+                            return
+                        }
+                        
                         guard let processedImage = self.processedImage else { return }
                         let imageSaver = ImageSaver()
                         
@@ -76,7 +143,8 @@ struct ContentView: View {
                         }
                         
                         imageSaver.errorHandler = { error in
-                            print("Error: \(error.localizedDescription)")
+                            self.alertMessage = error.localizedDescription
+                            self.showingAlert.toggle()
                         }
                         
                         imageSaver.writeToPhotoAlbum(image: processedImage)
@@ -93,29 +161,58 @@ struct ContentView: View {
             ActionSheet(title: Text("Select a filter"), buttons: [
                 .default(Text("Pixellate")) {
                     self.setFilter(CIFilter.pixellate())
+                    self.currentFilterAsString = "Pixellate"
                 },
                 .default(Text("Edges")) {
                     self.setFilter(CIFilter.edges())
+                    self.currentFilterAsString = "Edges"
                 },
                 .default(Text("Guassian Blur")) {
                     self.setFilter(CIFilter.gaussianBlur())
+                    self.currentFilterAsString = "Guassian Blur"
                 },
                 .default(Text("Sepia Tone")) {
                     self.setFilter(CIFilter.sepiaTone())
+                    self.currentFilterAsString = "Sepia Tone"
                 },
                 .default(Text("Unsharp Mask")) {
                     self.setFilter(CIFilter.unsharpMask())
+                    self.currentFilterAsString = "Unsharp Mask"
                 },
                 .default(Text("Vignette")) {
                     self.setFilter(CIFilter.vignette())
+                    self.currentFilterAsString = "Vignette"
                 },
                 .cancel()
             ])
+        }
+        .alert(isPresented: $showingAlert) {
+            Alert(title: Text("Error"), message: Text(self.alertMessage), dismissButton: .default(Text("OK")))
         }
     }
     
     func setFilter(_ filter: CIFilter) {
         currentFilter = filter
+        
+        withAnimation {
+            let inputKeys = currentFilter.inputKeys
+            if inputKeys.contains(kCIInputIntensityKey) {
+                self.showingIntensitySlider = true
+            } else {
+                self.showingIntensitySlider = false
+            }
+            if inputKeys.contains(kCIInputRadiusKey) {
+                self.showingRadiusSlider = true
+            } else {
+                self.showingRadiusSlider = false
+            }
+            if inputKeys.contains(kCIInputScaleKey) {
+                self.showingScaleSlider = true
+            } else {
+                self.showingScaleSlider = false
+            }
+        }
+        
         loadImage()
     }
     
@@ -133,10 +230,10 @@ struct ContentView: View {
             currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey)
         }
         if inputKeys.contains(kCIInputRadiusKey) {
-            currentFilter.setValue(filterIntensity * 200, forKey: kCIInputRadiusKey)
+            currentFilter.setValue(filterRadius * 200, forKey: kCIInputRadiusKey)
         }
         if inputKeys.contains(kCIInputScaleKey) {
-            currentFilter.setValue(filterIntensity * 10, forKey: kCIInputScaleKey)
+            currentFilter.setValue(filterScale * 10, forKey: kCIInputScaleKey)
         }
         
         guard let outputImage = currentFilter.outputImage else { return }
